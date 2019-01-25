c493f47bff093bcbfae29d8d0694514535b30ae5
figa-Oz <alpenizme@gmail.com>
2019-01-26 02:13:21 +0700

    adding syncronitation login to server

--- src/components/messages/InlineError.js
+++ src/components/messages/InlineError.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const InlineError = ({ text }) => (
-    <span style={{ color:'#982932' }}>{text}</span>
+    <span style={{ color:'#9f3a38' }}>{text}</span>
 );
 
 InlineError.propTypes = {
